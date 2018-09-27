9b7a71f2a3eacf77153595cb2af2085c8848b857
Cole Crutcher <colejcrutcher@gmail.com>
2018-09-27 18:17:59 -0500

    Checkpoint 5 Components: Library - Completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
-import Album from './components/Album';
+import Album from './components/album';
 
 class App extends Component {
   render() {
@@ -20,7 +20,7 @@ class App extends Component {
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
-          <Route path="/album" component={Album} />
+          <Route path="/album/:slug" component={Album} />
         </main>
       </div>
     );
