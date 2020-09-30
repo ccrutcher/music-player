39f719b09eb59315a1b0a230d4b9fff7af70aebd
Cole Crutcher <colejcrutcher@gmail.com>
2020-09-29 19:34:17 -0500

    Cleaned up slightly

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-          <a className="navbar-brand" href="/">Bloc Jams</a>
+          <a className="navbar-brand" href="/">My Jams</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
--- src/data/albums.js
+++ src/data/albums.js
@@ -24,4 +24,27 @@ export default [{
       { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
-}];
+}, {
+    title: 'Coloring Book',
+    artist: 'Chance The Rapper',
+    releaseInfo: '2016 Brasstracks',
+    albumCover: '/assets/images/album_covers/00.jpg',
+    slug: 'coloring-book',
+    songs: [
+      { title: 'All We Got', duration: '264', audioSrc: '/assets/Chance/All We Got.mp3' },
+      { title: 'No Problem', duration: '305', audioSrc: '/assets/Chance/No Problem.mp3' },
+      { title: 'Summer Friends', duration: '290', audioSrc: '/assets/Chance/Summer Friends.mp3' },
+      { title: 'DRAM Sings Special', duration: '101', audioSrc: '/assets/Chance/DRAM Sings Special.mp3' },
+      { title: 'Blessings', duration: '222', audioSrc: '/assets/Chance/Blessings.mp3' },
+      { title: 'Same drugs', duration: '258', audioSrc: '/assets/Chance/Same Drugs.mp3' },
+      { title: 'Mixtape', duration: '293', audioSrc: '/assets/Chance/Mixtape.mp3' },
+      { title: 'Angels', duration: '206', audioSrc: '/assets/Chance/Angels.mp3' },
+      { title: 'Juke Jam', duration: '220', audioSrc: '/assets/Chance/Juke Jam.mp3' },
+      { title: 'All Night', duration: '142', audioSrc: '/assets/Chance/All Night.mp3' },
+      { title: 'How Great', duration: '337', audioSrc: '/assets/Chance/How Great.mp3' },
+      { title: 'Smoke Break', duration: '226', audioSrc: '/assets/Chance/Smoke Break.mp3' },
+      { title: 'Finish Line Drown', duration: '406', audioSrc: '/assets/Chance/Finish Line Drown.mp3' },
+      { title: 'Blessings 2', duration: '230', audioSrc: '/assets/Chance/Blessings 2.mp3' }
+    ]
+}
+];
