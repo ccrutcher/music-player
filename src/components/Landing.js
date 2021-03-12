4647fb2a35ee4236a73e9f686a51f8751f002be0
Cole <colejcrutcher@gmail.com>
2021-03-12 17:17:10 -0600

    Updated node-modules, edited landing message

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,9 +5,8 @@ const Landing = () => (
   <div>
     <div id='welcome'>Welcome to My Tunes!</div>
     <div id='landing-content'>
-      This Single Page Application was created as an excercise to experiment
-      with React and create a music player in the process. Click below to access
-      the library!
+      Only My Tunes has the hottest jams.
+      Click below to access the library!
     </div>
     <button className='library-btn'>
       <Link to='/library'>Library</Link>
